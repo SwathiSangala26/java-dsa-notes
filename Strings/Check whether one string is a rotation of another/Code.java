828260eaefdec0ff907b1f8fdbb98f3c71a89ca2
SwathiSangala26 <swathisangala5@gmail.com>
2026-01-02 19:50:58 +0530

    Create Code.java

---
+++ Strings/Check whether one string is a rotation of another/Code.java
@@ -0,0 +1,12 @@
+public class StringRotationCheck {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "cdab";
+
+        if (s1.length() == s2.length() && (s1 + s1).contains(s2)) {
+            System.out.println("Rotation");
+        } else {
+            System.out.println("Not a Rotation");
+        }
+    }
+}
